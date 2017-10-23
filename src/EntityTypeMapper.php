b6f12874286051c83460a955fe61624483ecf051
Peter Arnold <github@oribi.dk>
2017-10-23 22:31:13 +0200

    Refactoring EntityTypeMapper; Filter options list to exclude some types

--- src/EntityTypeMapper.php
+++ src/EntityTypeMapper.php
@@ -3,7 +3,7 @@
 namespace Drupal\ws_data_sync;
 
 use Drupal\Core\Entity\ContentEntityType;
-use Drupal\webprofiler\Entity\EntityManagerWrapper;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfo;
 
 /**
@@ -25,7 +25,7 @@ class EntityTypeMapper {
   ];
 
   /**
-   * @var \Drupal\webprofiler\Entity\EntityManagerWrapper
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
 
@@ -37,13 +37,13 @@ class EntityTypeMapper {
   /**
    * Constructs a new EntityTypeMapper object.
    */
-  public function __construct(EntityManagerWrapper $entity_type_manager, EntityTypeBundleInfo $entity_type_bundle_info) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfo $entity_type_bundle_info) {
     $this->entityTypeManager = $entity_type_manager;
     $this->allEntityTypeBundleInfo = $entity_type_bundle_info->getAllBundleInfo();
   }
 
   /**
-   * Generate grouped options for 'local' (entity type select) form element
+   * Generate grouped options for entity type select form element
    * @return array
    *
    * @see \Drupal\ws_data_sync\Form\FeedForm
@@ -52,9 +52,15 @@ class EntityTypeMapper {
     $type_options = [];
     $entity_types = $this->entityTypeManager->getDefinitions();
 
-    foreach ($entity_types as $label => $definition) {
-      if ($definition instanceof ContentEntityType && !in_array($label, $this->excluded_types)) {
-        $type_options[$definition->getLabel()->render()] = self::getTypeBundles($label);
+    /**
+     * @var string $id
+     * @var \Drupal\Core\Entity\EntityTypeInterface $entity_type
+     */
+    foreach ($entity_types as $id => $entity_type) {
+      if (self::typeIsSelectable($entity_type)) {
+        $grouping_label = $entity_type->getLabel()->render();
+        $grouped_options = self::getBundleOptions($id);
+        $type_options[$grouping_label] = $grouped_options;
       }
     }
 
@@ -63,18 +69,38 @@ class EntityTypeMapper {
 
 
   /**
-   * Generate actual option values with format "type:bundle"
-   * @param $type_label
+   * Generate selectable options with option values formatted "type:bundle"
+   *
+   * @param string $type_id
    *
    * @return array
    */
-  protected function getTypeBundles($type_label) {
-    $type_bundles_options = [];
-    $bundle_info = $this->allEntityTypeBundleInfo;
-    foreach ($bundle_info[$type_label] as $key => $label) {
-      $type_bundles_options[$type_label . ':' . $key] = $label['label'];
+  protected function getBundleOptions($type_id) {
+    $bundles_options = [];
+    foreach ($this->allEntityTypeBundleInfo[$type_id] as $bundle_id => $bundle_data) {
+      $bundles_options[$type_id . ':' . $bundle_id] = $bundle_data['label'];
     }
-    return $type_bundles_options;
+    return $bundles_options;
+  }
+
+
+  /**
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *
+   * @return boolean
+   */
+  protected function typeIsSelectable($entity_type) {
+    if (!$entity_type instanceof ContentEntityType) {
+      return FALSE;
+    }
+    if (in_array($entity_type->id(), $this->excluded_types)) {
+      return FALSE;
+    }
+    if (!key_exists($entity_type->id(), $this->allEntityTypeBundleInfo)) {
+      return FALSE;
+    }
+
+    return TRUE;
   }
 
 }
